b4c5bf0b71584ed170b8150f4c982b80dde2f1d9
brianngeno <bkn.ngeno@gmail.com>
2018-10-16 17:20:01 +0300

    updated reg form

--- awward/settings.py
+++ awward/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'award',
-    'bootstrap4'
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
