0df7a27ec59c910457222ff925a8d8863eb3519f
brianngeno <bkn.ngeno@gmail.com>
2018-10-17 11:31:55 +0300

    added project class

--- award/models.py
+++ award/models.py
@@ -19,8 +19,20 @@ class Profile(models.Model):
     Profile_photo = models.ImageField(upload_to = 'images/',blank=True)
     Bio = models.TextField(max_length = 50)
     user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
-    rating = models.ManyToManyField('Image', related_name='image',max_length=30)
+    rating = models.ManyToManyField('Project', related_name='image',max_length=30)
 
     def save_profile(self):
         self.save()
+class Project(models.Model):
+    screenshot = models.ImageField(upload_to = 'images/')
+    project_name = models.CharField(max_length =30)
+    project_url = models.TextField(max_length =40)
+    profile = models.ForeignKey(Profile, null = True,related_name='image')
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    user= models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     
+    class Meta:
+        ordering = ['-pk']
+
+    def save_image(self):
+        self.save()
