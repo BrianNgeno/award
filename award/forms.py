10b59f96c3eaa5edf1e063338d2d46641c4240c3
brianngeno <bkn.ngeno@gmail.com>
2018-10-17 16:05:33 +0300

    updated view function for uploading new project

--- award/forms.py
+++ award/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Project, Profile, Rate
+
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
--- award/views.py
+++ award/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Project,Profile
+from .forms import ProjectForm
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def home_page(request):
-    return render(request,'home.html')
+    uploadform = ProjectForm()
+    return render(request,'home.html',{'uploadform':uploadform})
 
+@login_required(login_url='/accounts/login')
+def upload_project(request):
+    if request.method == 'POST':
+        uploadform = ProjectForm(request.POST, request.FILES)
+        if uploadform.is_valid():
+            upload = uploadform.save(commit=False)
+            upload.profile = request.user.profile
+            upload.save()
+            return redirect('home_page', username=request.user)
+    else:
+        uploadform = ProjectForm()
+    return render(request,'home.html',{'uploadform':uploadform})
