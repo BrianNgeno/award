8d0390697868e17776341334f392dac5f0d971fb
brianngeno <bkn.ngeno@gmail.com>
2018-10-18 11:05:31 +0300

    added a search function

--- award/views.py
+++ award/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Project,Profile
 from .forms import ProjectForm
 from django.contrib.auth.models import User
+import datetime as dt
 
 def convert_dates(dates):
     # function that gets the weekday number for the date.
@@ -18,8 +19,9 @@ def convert_dates(dates):
 
 # Create your views here.
 def home_page(request):
+    date = dt.date.today()
     project = Project.objects.all()
-    return render(request,'home.html',{'project':project})
+    return render(request,'home.html',{'date':date,'project':project})
 
 @login_required(login_url='/accounts/login')
 def upload_project(request):
@@ -37,3 +39,17 @@ def upload_project(request):
 def view_project(request):
     project = Project.objects.get_all()
     return render(request,'home.html',{'project':project})
+
+def search_results(request):
+    category= Categorys.objects.all()
+    location= Location.objects.all()
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_image = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-images/search.html',{"message":message,"images": searched_image,'category':category,"location":location})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request,'/search.html',{"message":message})
