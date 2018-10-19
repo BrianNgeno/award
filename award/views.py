02acfc9fdf08b3bb7ddd9b4b39b79611b11cefc4
brianngeno <bkn.ngeno@gmail.com>
2018-10-19 16:20:09 +0300

    added a post method

--- award/views.py
+++ award/views.py
@@ -8,6 +8,7 @@ import datetime as dt
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import ProfileSerializer
+from rest_framework import status
 
 def convert_dates(dates):
     # function that gets the weekday number for the date.
@@ -127,3 +128,10 @@ class ProfileList(APIView):
         all_profile = Profile.objects.all()
         serializers = ProfileSerializer(all_profile, many=True)
         return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
